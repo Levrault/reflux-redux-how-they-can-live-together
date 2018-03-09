ab747a3c4b82beceb645e0fd90a9fe93818b698f
Luc-Frédéric Langis <lucf.langis@gmail.com>
2018-03-09 18:38:04 +0100

    feat(button): add a simple background color transition

--- src/common/Button.js
+++ src/common/Button.js
@@ -2,14 +2,21 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   background: transparent;
+  font-weight: bold;
   flex:1;
   color: ${props => (props.color ? props.color : '#00cec9')};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid ${props => (props.color ? props.color : '#00cec9')};
-;
   border-radius: 3px;
+  transition: .25s;
+
+  &:hover {
+    background: ${props => (props.color ? props.color : '#00cec9')};
+    transition: .25s;
+    color: white;
+  }
 `;
 
 export default Button;
