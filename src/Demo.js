ee92e0fd2e1b423b728d67684d6e11113335c238
Luc-Frédéric Langis <lucf.langis@gmail.com>
2018-03-12 17:45:49 +0100

    fix(demo): remove unused component

--- src/Demo.js
+++ src/Demo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import RefluxButtons from './reflux/RefluxButtons';
 import ReduxButtonsContainer from './redux/ReduxButtonsContainer';
