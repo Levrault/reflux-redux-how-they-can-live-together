33f0c05b0c219c275136033e5696e41c0893503e
Luc-Frédéric Langis <lucf.langis@gmail.com>
2018-03-12 17:39:26 +0100

    feat(article): add an article to explain the demo page

---
+++ src/Demo.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import RefluxButtons from './reflux/RefluxButtons';
+import ReduxButtonsContainer from './redux/ReduxButtonsContainer';
+import CombinedStoreContainer from './combined/CombinedStoreContainer';
+import CombinedButtons from './combined/CombinedButtons';
+import RefluxStore from './reflux/RefluxStore';
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  border: 1px solid #000000d6;
+  border-radius: 8px;
+  margin-bottom: 32px;
+`;
+
+/**
+ * Demo component
+ */
+const Demo = () => (
+  <Wrapper>
+    <CombinedStoreContainer tag="Counter" reflux={RefluxStore} options={{ name: 'combined', value: 0 }} />
+    <ButtonWrapper>
+      <RefluxButtons />
+      <CombinedButtons onIncrement={RefluxStore.onIncrement} />
+      <ReduxButtonsContainer />
+    </ButtonWrapper>
+  </Wrapper>
+);
+
+export default Demo;
--- src/Demo.test.js
+++ src/Demo.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/ReduxStore';
-import Mock from './Mock';
+import Demo from './Demo';
 
 describe('Redux Store', () => {
   it('should renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Provider store={store}><Mock /></Provider>, div);
+    ReactDOM.render(<Provider store={store}><Demo /></Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
